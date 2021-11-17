5ea209af6f9b3e542d222ef1704541d0e0044107
Zamir Kanji <zamirkanji@comcast.net>
2021-11-16 23:03:15 -0800

    final update 11.16.21 - need to figure out how to properly randomize every time a mouseenter event happens (over a div)

--- app/app.js
+++ app/app.js
@@ -3,19 +3,22 @@ const grid = document.querySelector(".grid");
 const clearBtn = document.querySelector('.clear-btn');
 const gridText = document.querySelector('.grid-txt');
 const colorBtn = document.querySelectorAll('.color-btn');
+const blackBtn = document.querySelector('.black-color');
+const rgbBtn = document.querySelector('.rgb-color');
 
-colorBtn.forEach((b) => {
-    b.addEventListener('click', () => {
-        console.log("test");
-    })
-})
+// colorBtn.forEach((b) => {
+//     b.addEventListener('click', () => {
+//         console.log("test");
+//         // if (blackBtn)
+//     })
+// })
+const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
 
 let movedSlider = false; //onpage load
 
 const gridHeight = grid.clientHeight;
 const gridWidth = grid.clientWidth;
-
-
 
 // window.addEventListener('load', defaultGrid);
 
@@ -29,6 +32,11 @@ const gridWidth = grid.clientWidth;
 // }
 
 let run = false;
+
+
+defaultGrid();
+
+
 
 function defaultGrid (s) {
     // console.log(s);
@@ -45,9 +53,6 @@ function defaultGrid (s) {
     }  
     hoverGrid();
 }
-// getHW();
-defaultGrid();
-
 
 function checkValue (v) {
     movedSlider = true; 
@@ -74,12 +79,7 @@ function getArea (v) {
     // console.log(w, h);
     createGrid(w, h, v);  
 }
-
-
-
-//500px 500px is fixed height and width 
-//if user selects 32, then 32 x 32 to fill 500px 
-    
+ 
 function createGrid(w, h, v) {
     run = true;
     console.log(w, h);
@@ -98,10 +98,36 @@ function createGrid(w, h, v) {
 
 function hoverGrid() {
     const divsAll = document.querySelectorAll('.boxes');
+
+    rgbBtn.addEventListener('click', () => {
+        divsAll.forEach((d) => {
+            d.addEventListener('mouseenter', () => {
+              console.log("forEach worked");
+              d.style.backgroundColor = "#" + randomColor;
+            //   color.innerHTML = "#" + randomColor;
+            });
+        });
+    });
+
+    blackBtn.addEventListener('click', () => {
+        divsAll.forEach((d) => {
+            d.addEventListener('mouseenter', () => {
+              console.log("forEach worked");
+              d.style.backgroundColor = "black";
+            });
+        }); 
+    })
     
     divsAll.forEach((d) => {
         d.addEventListener('mouseenter', () => {
           console.log("forEach worked");
+          d.style.backgroundColor = "black";
+        });
+      });                                                    
+    divsAll.forEach((d) => {
+        d.addEventListener('dblclick', () => {
+          console.log("forEach worked");
+          d.style.backgroundColor = "white";
         });
       });                                                    
 }
